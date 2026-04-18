6eb9333d5bbea04c1fe79d988f5408dbea69a6dd
Mustafa Faisal <mustafafaisal363@gmail.com>
2026-04-19 03:34:31 +0500

    solving random ahh error

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sofifa.net",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "media.api-sports.io",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
